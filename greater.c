f12aefba832bc54a1f48519504c736865593c023
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2017-08-29 15:50:05 +0530

    greater
    its a program

---
+++ greater.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c:
+clrscr();
+printf("enter the value for a,b,c):
+scanf("%d,%d,%d",&a,&b,&c);
+if(a>b&&a>c)
+{
+priintf("a is greater");
+else if(b>c)
+printf("b is graeter");
+else
+printf("c is greater");
+}getch();
+}
