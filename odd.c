e7002058d270d9038d05469a6e61185e0ac6d26e
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2017-08-29 14:58:17 +0530

    odd
    its a program

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a:
+clrscr();
+printf("enter the number for a");
+scanf("%d",&a);
+if(a%2==0)
+printf("the given number is even");
+else
+printf("the given numbber is odd");
+}
+getch();
+}
