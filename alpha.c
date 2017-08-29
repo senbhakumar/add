97d1f7ba592eacf0decdfeca123e5bcfeff30a45
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2017-08-29 15:12:58 +0530

    alpha
    its a program

---
+++ alpha.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+clrscr();
+printf("enter the value of a");
+scanf("%s",a);
+if(a='e'||a=='b')
+printf("the value of a is alphabet"):
+getch();
+}
