0f5c75f1b36aff106e3b75e4a9b3634870c09bc9
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2017-08-29 14:51:21 +0530

    add.c
    this is a program

---
+++ add.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the number for a");
+scanf("%d",&a);
+if(a<0)
+{
+printf("the given number is positive");
+else
+printf("the given number is negatyive");
+}
+getch();
+}
