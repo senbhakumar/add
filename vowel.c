00c3a0ce196c0be9138cfe89f5343f70e512381d
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2017-08-29 15:07:19 +0530

    vowel
    its a program

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+include<conio.h>
+
+void main()
+{
+char a:
+
+clrscr();
+printf("enter the value for a");
+scan("%s",a);
+if(a='a'||a=='e'||a=='i'||a==',o'||a=='u')
+printf("the value of ais a vowel"):
+getch();
+}
