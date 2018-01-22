a0bc167b395451fbfbc8e84f111c1f2731ef8a0e
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-01-22 16:14:09 +0530

    Create odd1.c
    pro

---
+++ odd1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    int num,m =20,n=40;
+    printf("display the Odd Numbers in a given range m to n:\n");
+    for (num = m; num <= n; num++)
+        {
+               if (num % 2 == 1)
+                  printf ("%d ", num);
+         }
