d54b4fd8ea76cd796cdc426bbf03d43d36dc2934
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-01-22 16:45:22 +0530

    Create multiles.c
    pro

---
+++ multiles.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+longint sum=0,i,n;
+        clrscr();
+        printf("\n Please Give The Value of N:  ");
+        scanf("%ld",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("\n 5 * %ld = %ld",i,5*i);
+            }
