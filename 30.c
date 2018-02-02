fb041c95c4b08524f4e8901c1bdf8ad4c89d22eb
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-02 16:47:26 +0530

    Create 30.c
    pro

---
+++ 30.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c,d,e,f;
+	printf("give the  1st time");
+	scanf("%d %d",&a,&b);
+	printf("give the 2nd time");
+	scanf("%d %d",&c,&d);
+	e=a-c;
+	f=b-d;
+	printf("%d %d",e,f);
+	
+	return 0;
+}
