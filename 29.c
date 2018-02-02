899a375d44fad434fc2401c5f6a240ebb3b45818
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-02 16:38:59 +0530

    Create 29.c
    pro

---
+++ 29.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int bal,minute,hour;
+printf("enter the time");
+scanf("%d",&time);
+hour=minute/60;
+priintf("hour");
+bal=minute-hour*60;
+printf("bal time");
+return 0;
+}
