7d5f34c785091f8483907df141159264ee56ce48
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-02 16:30:25 +0530

    Create 28.c
    pro

---
+++ 28.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int array[10];
+int i,j,temp;
+printf("givethe element of an array\n");
+for(i=0;i<10;i++)
+scanf("%d",&array[i]);
+printf("The elements with its index are");
+for(i=0;i<10;i++)
+printf("%d",array[i]);
+    return 0;
+
+}
