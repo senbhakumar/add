ec49e0d08842c0875ee4a442e3456b068c7ea129
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-05 16:42:50 +0530

    Create 38.c
    pro

---
+++ 38.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   int a = 23, b = 47;
+   int t;
+   printf("Before. a: %d, b: %d\n", a, b);
+   scanf("%d%d",&a,&b);
+   t = a;
+   a = b;
+   b = t;
+   printf("After.a: %d, b: %d\n", a, b);
+   return 0;
+}
