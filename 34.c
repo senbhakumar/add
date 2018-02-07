4a4015ef5b7697bbf24824677caa2c374850af45
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-07 16:08:58 +0530

    Create 34.c
    pro

---
+++ 34.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+char char a[125];
+int s=0;
+printf("nEnter string");
+scanf("%s",&a);   
+if (char[a]=='\n')
+{
+s++;
+}
+printf("give lines is  %d ",j);
+return 0;
+}
