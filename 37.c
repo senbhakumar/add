3249653437faac11db24c4a9f7c754b51a9fa7f3
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-07 16:57:56 +0530

    Create 37.c
    pro

---
+++ 37.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int 1stnum,2ndnum,temp;
+printf("Enter 1stnum ");
+scanf("%lf", &1stnum);
+printf("Enter 2ndnum: ");
+scanf("%lf",&2ndnum);
+temp=1stnum;
+1stnum=2ndnum;
+2ndnum=temp;
+printf("\nAfter swapping,1stnum = %.2lf\n",1stnum);
+printf("After swapping,2nd = %.2lf",2nd);
+return 0;
+}
