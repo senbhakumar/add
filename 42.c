c480296ca2e2fd34f82e3030d40e07412cabac33
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-09 16:29:48 +0530

    Create 42.c
    pro

---
+++ 42.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char s1[100],s2[100];
+int i,j,11,10;
+priintf("enter the value of s1,s2");
+scanf("%s %s",&s1,&s2);
+l1=strlen(s1);
+l0=strlen(s2);
+if(l0>l1)
+{
+    printf("%s is greater",s1);
+}
+else
+{
+    printf("%s is lesser string",s2);
+}
+}
