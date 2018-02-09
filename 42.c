4d0f0b45ab5c761e56b57ab58b13ebc18fe67473
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-09 16:34:37 +0530

    Update 42.c
    pro

--- 42.c
+++ 42.c
@@ -2,17 +2,18 @@
 int main()
 {
 char s1[100],s2[100];
-int i,j,11,10;
-priintf("enter the value of s1,s2");
+int i,j;
+printf("enter the value of s1,s2");
 scanf("%s %s",&s1,&s2);
-l1=strlen(s1);
-l0=strlen(s2);
-if(l0>l1)
+i=strlen(s1);
+j=strlen(s2);
+if(i>j)
 {
-    printf("%s is greater",s1);
+printf("%s is greater",s1);
 }
 else
 {
-    printf("%s is lesser string",s2);
-}
+ printf("%s is lesser string",s2);
+ }
+return 0;
 }
