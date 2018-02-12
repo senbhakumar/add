2c7fe3929b45b57a205b5da34c62084a4260810b
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-12 15:46:16 +0530

    Create 44.c
    pro

---
+++ 44.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if(num=10&&num<11)
+{
+printf("1 to 10");
+}
+else
+{
+printf("not 1 to 10");
+return 0;
+}
