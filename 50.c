8cb41331b8cb4679c307f663d45b0f7426a9609f
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-12 16:12:50 +0530

    Create 50.c
    pro

---
+++ 50.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter the num vallue");
+scanf("%d",&num);
+if(num%2==0)
+printf("give num is *2 %d",num);
+else
+printf("it is nt *2 %d",num);
+getch();
+}
