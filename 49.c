d72d6ef30aff42fc6afc05201cdc09405f459dd6
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-12 16:05:55 +0530

    Create 49.c
    pro

---
+++ 49.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum;
+clrscr();
+printf("enter the value of a,b");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("the sum of two values is %d",sum);
+getch();
+}
