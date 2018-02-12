916201d0b5af9070034e207f2f763d8f9fd15e70
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-12 15:56:22 +0530

    Update 44.c
    pro

--- 44.c
+++ 44.c
@@ -1,15 +1,16 @@
-include<stdio.h>
+#include<stdio.h>
 int main()
 {
 int num;
 printf("enter the number");
 scanf("%d",&num);
-if(num=10&&num<11)
+if(num==0&&num<=10)
 {
 printf("1 to 10");
 }
 else
-{
+
 printf("not 1 to 10");
+
 return 0;
 }
