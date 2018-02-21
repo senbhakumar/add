1aa23f25ae5fac42cda9b82a0004767d529c241b
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-21 16:40:48 +0530

    Create 58.c
    pro

---
+++ 58.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter the value of a,b");
+scanf("%d%d".&a,&b);
+}
+void swap(int a,int b)
+{
+int a,b,temp;
+printf("bfr swaping");
+temp=a;
+a=b;
+b=temp;
+printf("aftr swap %d%d,a.b);
+getch();
+}
