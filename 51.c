62eda76c3baffcdb34ed1ba835ff991e8e703ed6
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-21 15:40:00 +0530

    pro

---
+++ 51.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the num");
+scanf("%d",&num);
+if(num>=0)
+printf("disply the num %d",num)
+else
+prinf("dont disply %d",num);
+getch();
+}
