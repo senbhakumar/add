4c951f0ecd972522a5f6b445ab3c6b6d46869744
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-21 16:30:56 +0530

    Create 52.c
    pro

---
+++ 52.c
@@ -0,0 +1,27 @@
+#include<Mstdio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter the num");
+scanf("%d",&num);
+if(num<=10)
+{
+switch(0)
+{
+case 1:
+printf"one");
+break;
+case 2:
+printf("two");
+break;
+case 3:
+printf"three");
+break;
+default:
+exit(0);
+}
+printf("display");
+getch();
+}
+
