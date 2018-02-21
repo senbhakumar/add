e2c1a0e28998eab1bba257f76d2a86c1f203c48b
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-21 16:13:54 +0530

    Create 53.C
    pro

---
+++ 53.C
@@ -0,0 +1,12 @@
+#include<stdio.h.
+void main()
+{int num,a,b,c,d;
+printf("enter the num");
+scanf("%d",&num);
+a=num/100;
+b=num/100;
+c=num/10;
+d=num/10;
+printf("display %d%d%d%d",a,b,c,d);
+getch();
+}
