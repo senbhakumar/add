91188550aa038dec57f6142918a9523647e27a1d
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-23 16:28:11 +0530

    Create 60.c
    pro

---
+++ 60.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a=0,b=1,c.i,n;
+clrscr();
+printf("enter 0the n terms");
+scanf("%d",&n);
+for(i=0i<=n;i++)
+{
+a=b;
+b=c;
+c=a+b;
+printf("series is %d",c);
+}
+getch();
+}
