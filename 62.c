a77c7bded9a255b09794b322ef6461fcefcf6564
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-28 15:56:55 +0530

    Create 62.c
    pro

---
+++ 62.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+int a[10],i,num;
+clrscr();
+printf("enter the num");
+scanf("%d",&num);
+
+for(i=0;i<=a[10];i++)
+
+if(a[i]==0&&a[i]==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
