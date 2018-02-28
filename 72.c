1bc3b719cfe694edeff8629705ededbf9a94c29d
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-28 16:39:28 +0530

    Create 72.c
    pro

---
+++ 72.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+char b[100],a,e,o,u;
+int i,count=0;
+clrscr();
+printf("enter the value");
+scanf("%s",b);
+for(i=0;i<=b[i];i++)
+{
+if(b[i]==a &&b[i]==e &&b[i]==o &&b[i]==u &&b[i]==i)
+{
+printf("yes");
+count++;
+}
+else
+{
+printf("no");
+}
+getch();
+}}
