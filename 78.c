b51efdd3c3af9487e0b8ba78addf5c2cf6087a59
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-02-28 16:50:08 +0530

    Create 78.c
    pro

---
+++ 78.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter the num ");
+scanf("%d",&num);
+{
+if(num%13==0)
+{
+printf("yes %d",num);
+}
+else
+{
+printf("no %d",num);
+}
+getch();
+}}
