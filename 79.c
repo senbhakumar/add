f5de4f4669d6555cef23122aa310bebc01893cbc
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-03-02 16:23:30 +0530

    Create 79.c
    pro

---
+++ 79.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void  main()
+{
+int a=10,b=15,i=1,c;
+c=a*b;
+clrscr();
+for(i=0;i<1;i++)
+{
+if(c==i*i)
+{
+printf("yes",c);
+}
+else
+{
+printf("no",c);
+}}
+getch();
+}
