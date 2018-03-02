c22782286c130cf1dff6d445a943dce211df647e
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-03-02 16:21:37 +0530

    Create 76.c
    pro

---
+++ 76.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int n,a,i;
+clrscr();
+printf("enter the num\n");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+a=1;
+
+}}
+if(a==0)
+{
+printf("prime no");
+}
+else
+{
+printf("composite num\n");
+}
+getch();
+}
