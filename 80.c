c8402d8f1baf58750ddabbe62fb714279faffd88
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-03-02 16:41:39 +0530

    Create 80.c
    pro

---
+++ 80.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char a[100]="12345";
+int i=1;
+1==strlen(a);
+printf("enter the a value");
+scanf("%c",a);
+for(i=0;i<1;i++)
+{
+if(a[i]%2!=0)
+{
+printf("%c",a[i]);
+}
+
+getch();
+}}
