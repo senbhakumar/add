1786dc8d79161adbb7063f7f227d25ce2beed64b
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-03-09 16:05:34 +0530

    Create 85.cpp
    pro

---
+++ 85.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+int i,1;
+char s[20];
+cout<<"enter the string";
+cin>>s;
+1=strlen(s);
+for(i=0;i<=1;i++)
+{
+if(i%2==0)
+{
+cout<<"print"<<s[i];
+}
+cout<<"  "<<s[i];
+for(i=0;i<=1;i++)
+{
+if(i%2==1)
+{
+cout<<"disaply"<<s[i];
+}
+}
+getch();
+
+}}
