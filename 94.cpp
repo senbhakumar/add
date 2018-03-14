89a354fc3cb6dca8dc3e3b846bbdea746a3a8b06
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-03-14 16:26:57 +0530

    k number
    pro

---
+++ 94.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+int main()
+{
+int i,n,k,a[20];
+cout<<"enter the valuue";
+cin>>n;
+for(i=0;i<=n;i++)
+{
+cin>>a[i];
+}
+for(i=0;i<=n;i++)
+{
+if(a[i]==k)
+{
+cout<<"display"<<a[i];
+}
+}
+return 0;
+}
