099c2b5a3dfc40bc9a43cc61e07c3515ac17bb52
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-03-14 16:15:12 +0530

    cuboid
    pro

---
+++ 91.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+int main()
+{
+int l,b,h,tsa;
+cout<<"enter the lenght,breadth,height";
+cin>>l>>b>>h;
+cout<<"volume of the cuboid is"<<l<<b<<h;
+tsa=2*((l*b)+(b*h)+(h*l));
+cout<<"area of the cuboid ",tsa;
+return 0;
+}
