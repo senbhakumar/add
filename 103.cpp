95f9e800a2c2057d908b0c1dcb5317ed02d9655b
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-03-19 15:49:06 +0530

    Create 103.cpp
    pro

---
+++ 103.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+#include<string.h>
+int main()
+{
+char s[50];
+int i;
+cout<<"enter the value";
+cin>>s;
+for(i=0;s[i]='\o';i++)
+
+if(i==0)
+{
+s[i]=big(s[i]));
+}
+else
+
+if(s[i]==' ')
+{
+s[i+1]=big(s[i+1]);
+}
+cout<<"display"<<s;
+return 0;
+}
