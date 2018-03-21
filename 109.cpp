432d02912003839c7deff409a8c65677215d97d1
senbhakumar <31240270+senbhakumar@users.noreply.github.com>
2018-03-21 15:51:10 +0530

    Create 109.cpp
    pro

---
+++ 109.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+int main()
+{
+int a[100],i,min,n;
+cout<<"enter the element";
+for(i=1;i<=10;i++)
+{
+cin>>a[i];
+}
+if(a[i]<min)
+{
+min=a[i];
+}
+cout<<"minimun element is"<<min;
+return 0;
+}
